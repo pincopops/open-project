69dc8b89401f1e48ed109ee9b8865f9a61e7364f
Elio Scaletta <elioscal@hotmail.it>
2023-03-05 22:43:38 +0100

    Calculator component added.

--- test/src/App.js
+++ test/src/App.js
@@ -3,14 +3,17 @@ import "./components_scss/_App.scss";
 import Greatings from "./components/Greatings";
 import Navbar from "./components/Navbar";
 import Counter from "./components/Counter";
+import Calculator from "./components/Calculator";
+
 
 
 function App(props) {
     return (
         <div id="main-container">
             <Navbar />
-            <Greatings name="Elio" />
+            <Greatings name="irene" />
             <Counter />
+            <Calculator />
 
 
         </div>
---
+++ test/src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Calculator () {
+    return(
+        <div>
+            <label>Insert your numbers</label>
+
+        </div>
+    )
+}
+
+export default Calculator
